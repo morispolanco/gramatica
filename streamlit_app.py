aff56749ffdbb2d8f68044ce2f74b1b40323b92c
morispolanco <morispolanco@gmail.com>
2023-04-02 05:04:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,9 +47,27 @@ def corregir_gramatica_y_estilo(texto_ensayo):
     ensayo_corregido = ' '.join(correcciones)
     return ensayo_corregido
 
-    st.download_button(
-        label="Descargar Ensayo Corregido",
-        data=open("ensayo_corregido.txt", "rb"),
-        file_name="ensayo_corregido.txt",
-        mime="text/plain",
-    )
+st.title("Corrector de Gramática y Estilo")
+archivo_subido = st.file_uploader("Sube tu ensayo", type=["txt", "docx", "pdf"])
+
+if archivo_subido is not None:
+    deteccion = chardet.detect(archivo_subido.read())
+    codificacion = deteccion['encoding'] or 'latin-1'
+    archivo_subido.seek(0)
+    
+    texto_ensayo = archivo_subido.read().decode(codificacion)
+
+    if st.button("Corregir Gramática y Estilo"):
+        ensayo_corregido = corregir_gramatica_y_estilo(texto_ensayo)
+        st.write("Ensayo corregido:")
+        st.write(ensayo_corregido)
+
+        with open("ensayo_corregido.txt", "w") as f:
+            f.write(f"Ensayo original:\n\n{texto_ensayo}\n\nEnsayo corregido:\n\n{ensayo_corregido}")
+
+        st.download_button(
+            label="Descargar Ensayo Corregido",
+            data=open("ensayo_corregido.txt", "rb"),
+            file_name="ensayo_corregido.txt",
+            mime="text/plain",
+        )
